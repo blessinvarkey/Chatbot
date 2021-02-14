30e5065f74933da5ac21fccf97cf09f8434b750c
Blessin Varkey <blessinvarkey@gmail.com>
2021-02-14 23:55:11 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,3 +4,5 @@ Gets input from the user
 Processes the input and returns value that generated the highest confidence value from logic adapters
 Returns response to the user
 '''
+from flask import Flask, render_template, request
+from chatterbot import Chatterbot
