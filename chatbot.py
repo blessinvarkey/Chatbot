495c431bc60e6b4e857fcfd8292eac5a09cb661e
Blessin Varkey <blessinvarkey@gmail.com>
2021-02-14 23:59:11 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -13,3 +13,9 @@ app = Flask(__name__)
 english_bot = ChatBot('Chatterbot', storage_adapter="chatterbot.storage.SQLStorateAdapter")
 trainer = ChatterbotCorpusTrainer(english_bot)
 trainer.train("chatterbot.corpus.english")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("get")
