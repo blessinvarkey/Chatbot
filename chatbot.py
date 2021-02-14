71a13e741a4bc794b86ca595438f880ebd816be5
Blessin Varkey <blessinvarkey@gmail.com>
2021-02-14 23:48:29 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,6 @@
+'''
+Using the Chatterbot -corpus
+Gets input from the user
+Processes the input and returns value that generated the highest confidence value from logic adapters
+Returns response to the user
+'''
