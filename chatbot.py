cabdfaa4995fda8fc687725d1389d892f69e7267
Blessin Varkey <blessinvarkey@gmail.com>
2021-02-14 23:57:37 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,3 +6,10 @@ Returns response to the user
 '''
 from flask import Flask, render_template, request
 from chatterbot import Chatterbot
+from chatterbot.trainers import ChatterbotCorpusTrainer
+
+app = Flask(__name__)
+
+english_bot = ChatBot('Chatterbot', storage_adapter="chatterbot.storage.SQLStorateAdapter")
+trainer = ChatterbotCorpusTrainer(english_bot)
+trainer.train("chatterbot.corpus.english")
